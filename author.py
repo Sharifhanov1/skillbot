cf5a9199b7366e77293e9de813c013a8dec76b35
Andrey_s <andrey.sharifhanov@mail.ru>
2025-05-04 21:39:50 +0500

    add block bot

--- author.py
+++
@@ -1,4 +0,0 @@
-from telegram import Update
-
-async def author_info(update: Update):
-    await update.message.reply_text("Информация о боте: бот создан в рамках учебы на SkillBOX")
--- crm.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-async def handle_crm(update: Update, context: CallbackContext):
-    """Обработчик для CRM-системы."""
-    await update.message.reply_text("Здесь будет интеграция с CRM-системой. 🛠️")
